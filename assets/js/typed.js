3c83bf6ba3ee1195a7badf8ee775b18134dad680
lucasrenandns <lucasrenandns96@gmail.com>
2023-02-03 20:34:16 -0300

    Typewriter Effect

---
+++ assets/js/typed.js
@@ -0,0 +1,29 @@
+// Strict mode
+"use strict"
+
+// Elements
+const words = ["Hello, My name is Lucas Renan", "I from Brazil, SP", "I study Information Technology"]
+let counter = 0
+let index = 0
+let currentText = ""
+let letter = ""
+
+// Function for typed effect
+const typed = () => {
+    const typeElement = document.getElementById("typed-text")
+
+    currentText = words[counter]
+    letter = currentText.slice(0, ++index)
+    typeElement.textContent = letter
+    
+    if(letter.length === currentText.length) {
+        counter++
+        index = 0
+    }
+    if(counter === words.length) {
+        counter = 0
+    }
+    setTimeout(typed, 135)
+}
+// Calling the function in the document
+typed()
